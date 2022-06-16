9f33e84c07a6fd785ab6661fedf70514a758273a
Filipe Soares <filipecarlossoares@gmail.com>
2022-06-16 18:51:56 -0300

    initial project structure

---
+++ src/main/java/com/simpledev/ecommerce/EcommerceCleanArchiApplication.java
@@ -0,0 +1,13 @@
+package com.simpledev.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCleanArchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCleanArchiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpledev/ecommerce/EcommerceCleanArchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpledev.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCleanArchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
