0119532dd3a6641f58d01bee0a150b5510645f5d
Filipe Soares <filipecarlossoares@gmail.com>
2022-07-09 12:51:12 -0300

    create getOrderByCode controller

--- src/main/java/com/simpledev/ecommerce/infra/controller/OrderController.java
+++ src/main/java/com/simpledev/ecommerce/infra/controller/OrderController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.simpledev.ecommerce.application.GetOrder;
 import com.simpledev.ecommerce.application.GetOrders;
 import com.simpledev.ecommerce.application.dto.OrderOutput;
 import com.simpledev.ecommerce.domain.repository.OrderRepository;
@@ -17,14 +19,21 @@ public class OrderController {
 
 	private final OrderRepository orderRepository;
 	private GetOrders getOrders;
+	private GetOrder getOrder;
 
 	public OrderController(OrderRepository orderRepository) {
 		this.orderRepository = orderRepository;
 		this.getOrders = new GetOrders(this.orderRepository);
+		this.getOrder = new GetOrder(this.orderRepository);
 	}
 
 	@GetMapping
 	public ResponseEntity<List<OrderOutput>> getOrders() {
 		return ResponseEntity.ok(getOrders.execute());
+	}
+
+	@GetMapping("/{code}")
+	public ResponseEntity<OrderOutput> getOrderByCode(@PathVariable String code) {
+		return ResponseEntity.ok(getOrder.execute(code));
 	}
 }
